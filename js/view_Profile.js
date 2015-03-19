9b50fe6fe5480a4fccd073588141b1567fa304fd
LeRoy Ellis <leroy.w.ellis@gmail.com>
2015-03-19 16:53:06 -0600

    Working on RESTful API stuff.

--- js/view_Profile.js
+++ js/view_Profile.js
@@ -8,10 +8,31 @@ View_Profile = { Profile: {
         var options = {
 			method: 'GET',
             url: 'services/player.php',
+            //url: 'services/v1/example/',
             data: 'pid=' + pid,
             dataType: 'json'
         };
 
+/*
+		$.ajax( options )
+		 .success( function( data ) {
+             console.log( data );
+             data.url = 'http://i404.photobucket.com/albums/pp126/ion_the_jester/dgir.gif';
+             $( '#playerPhoto' ).html( '<img src="' + data.url + '">' );
+             $( '#playerName' ).html( data.firstname + ' ' + data.middlename + ' ' + data.lastname );
+             $( '#playerNumber' ).html( data.jersey );
+
+             HOCKEY.setView( '#profileView' );
+         })
+		 .ajaxError( function( event, jqXHR, ajaxSettings, thrownError ) {
+             data.url = 'http://i404.photobucket.com/albums/pp126/ion_the_jester/dgir.gif';
+             $( '#playerPhoto' ).html( '<img src="' + data.url + '">' );
+             $( '#playerName' ).html( "Error!" );
+             $( '#playerNumber' ).html( thrownError );
+
+             HOCKEY.setView( '#profileView' );
+		 });
+		 */
         $.when( $.ajax( options ) )
          .then( function( data ) {
              console.log( data );
@@ -21,6 +42,14 @@ View_Profile = { Profile: {
              $( '#playerNumber' ).html( data.jersey );
 
              HOCKEY.setView( '#profileView' );
-         });
+         })
+		 .fail( function( event, jqXHR, ajaxSettings, thrownError ) {
+             url = 'http://i404.photobucket.com/albums/pp126/ion_the_jester/dgir.gif';
+             $( '#playerPhoto' ).html( '<img src="' + url + '">' );
+             $( '#playerName' ).html( jqXHR );
+             $( '#playerNumber' ).html( ajaxSettings );
+
+             HOCKEY.setView( '#profileView' );
+		 });
     },
 }}
--- services/services.php
+++ services/services.php
@@ -1,8 +1,33 @@
-[
-    player: {
-        firstName: "LeRoy",
-        middleName: "W.",
-        lastName: "Ellis",
-        number: "77"
-    },
-]
+<?php
+require_once 'RESTService.php';
+
+class p
+{
+	public $firstName = "";
+	public $lastName = "";
+	public $middleName = "";
+	public $number = "";
+}
+
+class Services extends RESTService
+{
+    protected $User;
+
+    public function __construct( $request, $origin )
+	{
+        parent::__construct( $request );
+    }
+
+	protected function example()
+	{
+		if( $this->method == 'GET' )
+		{
+			$player = array("player" => array( "firstName" => "LeRoy", "middleName" => "W.", "lastName" => "Ellis", "number" => "7" ) );
+			return $player;
+		}
+		else
+		{
+		    return "Only accepts GET requests";
+		}
+	}
+}
